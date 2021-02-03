06f7cd9533a302d66197547fca2c5346f24e7d96
wtp5063 <wtp5063@gmail.com>
2021-02-03 15:37:40 +0900

    リンク編集

--- src/main/java/com/example/demo/SpringTestApplication.java
+++ src/main/java/com/example/demo/SpringTestApplication.java
@@ -2,12 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringTestApplication {
+public class SpringTestApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringTestApplication.class, args);
 	}
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(SpringTestApplication.class);
+	}
 }
